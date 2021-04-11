7a81dace622de623f9dec7db3b846e61920a9a04
marco-ballario <s286154@studenti.polito.it>
2021-04-11 23:08:57 +0200

    feat: first lab completed

--- lab1/e2/e2.js
+++ lab1/e2/e2.js
@@ -1,15 +1,18 @@
 'use strict';
 const dayjs = require('dayjs');
+const sqlite = require('sqlite3');
+
 
 function Task(id, description, optionals){
     this.id = id;
     this.description = description;
-    this.urgent = optionals.urgent ? optionals.urgent : false;
-    this.private = optionals.private ? optionals.private : true;
+    this.urgent = optionals.urgent === true ? true : false;
+    this.private = optionals.private === false ? false : true;
     this.deadline = optionals.deadline ? optionals.deadline : null;
 }
 
 function TaskList(){
+    this.db = new sqlite.Database('./lab1/e2/tasks.db', (err) => { if (err) throw err; });
     this.list = [];
 
     this.sortAndPrint = function(){
@@ -45,18 +48,113 @@ function TaskList(){
         }
         
     };
+
+    this.loadAndPrintAllTasks = () => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM tasks;';
+            this.db.all(query, [], (err, rows) => {
+                if(err){
+                    reject(err);
+                }else{
+                    const tempList = [];
+                    for(let el of rows){
+                        let urg = el.urgent == 0 ? false : true;
+                        let pri = el.private == 0 ? false : true;
+                        let dea = dayjs(el.deadline);
+                        let t = new Task(el.id, el.description, { urgent: urg, private: pri, deadline: dea });
+                        tempList.push(t);
+                    }
+                    this.list = tempList;
+                    this.printTaskList();
+                    resolve(rows);
+                }
+            });
+        });
+    };
+
+    this.loadAndPrintTasksAfter = (date) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM tasks WHERE DATE(deadline) > DATE(?);';
+            this.db.all(query, [date], (err, rows) => {
+                if(err){
+                    reject(err);
+                }else{
+                    const tempList = [];
+                    for(let el of rows){
+                        let urg = el.urgent == 0 ? false : true;
+                        let pri = el.private == 0 ? false : true;
+                        let dea = dayjs(el.deadline);
+                        let t = new Task(el.id, el.description, { urgent: urg, private: pri, deadline: dea });
+                        tempList.push(t);
+                    }
+                    this.list = tempList;
+                    this.printTaskList();
+                    resolve(rows);
+                }
+            });
+        });
+    };
+    
+    this.loadAndPrintTasksWithText = (text) => {
+        return new Promise((resolve, reject) => {
+            const query = 'SELECT * FROM tasks WHERE description LIKE ?;';
+            this.db.all(query, ['%' + text + '%'], (err, rows) => {
+                if(err){
+                    reject(err);
+                }else{
+                    const tempList = [];
+                    for(let el of rows){
+                        let urg = el.urgent == 0 ? false : true;
+                        let pri = el.private == 0 ? false : true;
+                        let dea = dayjs(el.deadline);
+                        let t = new Task(el.id, el.description, { urgent: urg, private: pri, deadline: dea });
+                        tempList.push(t);
+                    }
+                    this.list = tempList;
+                    this.printTaskList();
+                    resolve(rows);
+                }
+            });
+        });
+    };
+
+    this.printTaskList = function(){
+
+        console.log("****** Tasks loaded: ******");
+        for(let el of this.list){
+            console.log("Id: " + el.id +
+                        ", Description: " + el.description +
+                        ", Urgent: " + el.urgent +
+                        ", Private: " + el.private +
+                        ", Deadline: " + (el.deadline ? el.deadline.format('MMMM DD, YYYY h A') : "<not defined>"));
+        }
+        
+    };
+    
 }
 
 
-const myTaskList = new TaskList();
-myTaskList.list.push( new Task("1", "laundry", {}) );
-myTaskList.list.push( new Task("2", "monday lab", { private: false, deadline: dayjs('2021-3-16T10:00') }) );
-myTaskList.list.push( new Task("3", "phone call", { urgent: true, private: false, deadline: dayjs('2021-3-8T16:20') }) );
+async function main(){
 
-console.log("Task list:")
-console.log(myTaskList.list);
+    const myTaskList = new TaskList();
 
-myTaskList.sortAndPrint();
-myTaskList.filterAndPrint();
+    /*
+    myTaskList.list.push( new Task("1", "laundry", {}) );
+    myTaskList.list.push( new Task("2", "monday lab", { private: false, deadline: dayjs('2021-3-16T10:00') }) );
+    myTaskList.list.push( new Task("3", "phone call", { urgent: true, private: false, deadline: dayjs('2021-3-8T16:20') }) );
+    
+    console.log("Task list:")
+    console.log(myTaskList.list);
 
-debugger;
+    myTaskList.sortAndPrint();
+    myTaskList.filterAndPrint();
+    */
+
+    await myTaskList.loadAndPrintAllTasks();
+    await myTaskList.loadAndPrintTasksAfter('2021-03-07');
+    await myTaskList.loadAndPrintTasksWithText('lau');
+
+}
+
+main();
+
